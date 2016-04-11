f653f744ca25c1356b4647fcd05f56701625876f
Jonathan Shih <joncshih@gmail.com>
2016-04-11 02:39:33 -0700

    Reservation model tests

--- test/models/reservation_test.exs
+++ test/models/reservation_test.exs
@@ -3,7 +3,7 @@ defmodule CollaborativeCalendar.ReservationTest do
 
   alias CollaborativeCalendar.Reservation
 
-  @valid_attrs %{date: "2010-04-17", user: "some content"}
+  @valid_attrs %{date: "2010-04-17", user: "asdf"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
@@ -14,5 +14,26 @@ defmodule CollaborativeCalendar.ReservationTest do
   test "changeset with invalid attributes" do
     changeset = Reservation.changeset(%Reservation{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "user required" do
+    changeset = Reservation.changeset(%Reservation{}, %{date: "2016-04-11"})
+    refute changeset.valid?
+  end
+
+  test "date required" do
+    changeset = Reservation.changeset(%Reservation{}, %{user: "asdf"})
+    refute changeset.valid?
+  end
+
+  test "date uniqueness" do
+    changeset = Reservation.changeset(
+      %Reservation{},
+      %{date: "2016-04-11", user: "asdf"})
+    changeset |> CollaborativeCalendar.Repo.insert
+    reservation = changeset |> CollaborativeCalendar.Repo.insert
+
+    assert {:error, invalidChangeset} = reservation
+    assert invalidChangeset.errors[:date] == "has already been taken"
   end
 end
